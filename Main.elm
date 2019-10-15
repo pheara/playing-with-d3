fdcd46e6de63ca761fe2d2c82c1029f9bfeb1591
moru <e1028092@student.tuwien.ac.at>
2019-10-15 22:58:49 +0200

    Now has elm stuff.

---
+++ Main.elm
@@ -0,0 +1,18 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (h1, text)
+import Axis
+-- import DateFormat
+-- import SampleData exposing (timeSeries)
+-- import Scale exposing (BandConfig, BandScale, ContinuousScale, defaultBandConfig)
+-- import Time
+-- import TypedSvg exposing (g, rect, style, svg, text_)
+-- import TypedSvg.Attributes exposing (class, textAnchor, transform, viewBox)
+-- import TypedSvg.Attributes.InPx exposing (height, width, x, y)
+-- import TypedSvg.Core exposing (Svg, text)
+-- import TypedSvg.Types exposing (AnchorAlignment(..), Transform(..))
+
+
+main =
+  h1 [] [ text "Hello, Elm!" ]
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import * as d3 from "d3";
+import { Elm } from "./Main.elm";
 
 console.log(d3);
+
+console.log(document.querySelector(".elm-mount"));
+
+var x = Elm.Main.init({
+  node: document.querySelector(".elm-mount")
+});
+console.log(x);
