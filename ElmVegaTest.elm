7fa84b108a2732f1ca45a05bd3a93a1e5d95a29b
rsinger <renee.singer@researchstudio.at>
2019-10-16 15:10:22 +0200

    Some tinkering with vega and elm.

---
+++ ElmVegaTest.elm
@@ -0,0 +1,40 @@
+port module ElmVegaTest exposing (elmToJS)
+
+-- from https://github.com/gicentre/elm-vegalite/tree/master/docs/helloWorld
+
+import Platform
+import VegaLite exposing (..)
+
+
+myVis : Spec
+myVis =
+    let
+        data =
+            dataFromColumns []
+                << dataColumn "a" (strs [ "C", "C", "D", "E" ])
+                << dataColumn "b" (nums [ 2, 7, 1, 2 ])
+
+        enc =
+            encoding
+                << position X [ pName "a", pMType Nominal ]
+                << position Y [ pName "b", pMType Quantitative, pAggregate opMean ]
+    in
+    toVegaLite [ data [], bar [], enc [] ]
+
+
+
+{- The code below is boilerplate for creating a headless Elm module that opens
+   an outgoing port to JavaScript and sends the Vega-Lite spec (myVis) to it.
+-}
+
+
+main : Program () Spec msg
+main =
+    Platform.worker
+        { init = always ( myVis, elmToJS myVis )
+        , update = \_ model -> ( model, Cmd.none )
+        , subscriptions = always Sub.none
+        }
+
+
+port elmToJS : Spec -> Cmd msg
--- index.js
+++ index.js
@@ -1,11 +1,69 @@
 import * as d3 from "d3";
-import { Elm } from "./Main.elm";
+import * as ElmMain from "./Main.elm";
+import * as vega from "vega";
+import * as vegaLite from "vega-lite";
+import * as vegaEmbed from "vega-embed";
+import * as irisData from "./iris.json";
+import * as ElmVegaTest from "./ElmVegaTest.elm";
 
 console.log(d3);
 
-console.log(document.querySelector(".elm-mount"));
+console.log("vega: ", vega);
+console.log("vegaLite: ", vegaLite);
+console.log("vegaEmbed: ", vegaEmbed);
 
-var x = Elm.Main.init({
+var elmApp = ElmMain.Elm.Main.init({
   node: document.querySelector(".elm-mount")
 });
-console.log(x);
+console.log(elmApp);
+
+const irisScatterPlotConf = {
+  description: "A description",
+  data: { values: irisData },
+  mark: "point",
+  encoding: {
+    x: {
+      field: "petalLength",
+      type: "quantitative"
+    },
+    y: {
+      field: "petalWidth",
+      type: "quantitative"
+    }
+  }
+};
+const testPlotConf = {
+  data: {
+    values: [
+      { a: "A", b: 28 },
+      { a: "B", b: 55 },
+      { a: "C", b: 43 },
+      { a: "D", b: 91 },
+      { a: "E", b: 81 },
+      { a: "F", b: 53 },
+      { a: "G", b: 19 },
+      { a: "H", b: 87 },
+      { a: "I", b: 52 }
+    ]
+  },
+  mark: "bar",
+  encoding: {
+    x: { field: "a", type: "ordinal" },
+    y: { field: "b", type: "quantitative" }
+  }
+};
+// vegaEmbed
+//   .default(
+//     "#vega-mount",
+//     // irisScatterPlotConf,
+//     testPlotConf,
+//     { actions: false }
+//   )
+//   .catch(console.warn);
+
+ElmVegaTest.Elm.ElmVegaTest.init().ports.elmToJS.subscribe(function(specs) {
+  // Change actions to true to display links to source, editor and image.
+  vegaEmbed
+    .default("#vega-mount", specs, { actions: false })
+    .catch(console.warn);
+});
